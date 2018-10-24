5eb9be5c8c2e838e417f2606908ed0f3186bd512
Matti Schneider <hi@mattischneider.fr>
2018-10-24 22:36:36 +0200

    Show audio files in an HTML table

--- audio/generate-sentences.js
+++ audio/generate-sentences.js
@@ -2,6 +2,7 @@ const exec = require('child_process').exec
 const fs = require('fs')
 
 
+const DESTINATION = '../web/audio'
 const MARKERS = {
 	bigDot:	'•',
 	dash:	'-',
@@ -14,18 +15,20 @@ const MARKERS = {
 
 for (let markerName in MARKERS) {
 	let marker = MARKERS[markerName]
+	let aiffFile = `${DESTINATION}/${markerName}.aiff`
+	let m4aFile = `${DESTINATION}/${markerName}.m4a`
+
 	let sentence = `Merci à tou${marker}te${marker}s les contributeur${marker}trices qui sont venu${marker}e${marker}s.`
-	let aiffFile = `${markerName}.aiff`
 
 	exec(`say --output-file=${aiffFile} ${sentence}`, err => {
 		if (err) return console.error(err)
 
 		console.log(`Wrote ${aiffFile}, converting to AAC`)
 
-		exec(`ffmpeg -y -i ${aiffFile} -c:a aac -b:a 64k ${markerName}.m4a`, (err, stdout, stderr) => {
+		exec(`ffmpeg -y -i ${aiffFile} -c:a aac -b:a 64k ${m4aFile}`, (err, stdout, stderr) => {
 			if (err) return console.error(err)
 
-			console.log(`Converted ${aiffFile}, wrote ${markerName}.m4a`)
+			console.log(`Converted ${aiffFile}, wrote ${m4aFile}`)
 			fs.unlink(aiffFile, err => {
 				if (err) return console.error(err)
 
