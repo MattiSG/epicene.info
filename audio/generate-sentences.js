a42021838b76772df857664cb948d43339f3e7cb
Matti Schneider <hi@mattischneider.fr>
2018-10-24 22:10:14 +0200

    Generate synthesis as AIFF

--- audio/generate-sentences.js
+++ audio/generate-sentences.js
@@ -9,10 +9,10 @@ for (let markerName in MARKERS) {
 	let marker = MARKERS[markerName]
 	let sentence = `Merci à tou${marker}te${marker}s les contributeur${marker}trices qui sont venu${marker}e${marker}s.`
 
-	require('fs').writeFile(`${markerName}.txt`, sentence, err => {
+	require('child_process').exec(`say --output-file=${markerName}.aiff ${sentence}`, err => {
 		if (err)
 			console.error(err)
 		else
-			console.log(`Wrote ${markerName}.txt`)
+			console.log(`Wrote ${markerName}.aiff`)
 	})
 }
