c90f993173c45d85e43c5e3dcc85a2d7835fc497
Matti Schneider <hi@mattischneider.fr>
2018-10-24 22:23:31 +0200

    Generate voice synthesis in AAC

--- audio/generate-sentences.js
+++ audio/generate-sentences.js
@@ -1,3 +1,7 @@
+const exec = require('child_process').exec
+const fs = require('fs')
+
+
 const MARKERS = {
 	none: '',
 	median: '·',
@@ -9,11 +13,22 @@ const MARKERS = {
 for (let markerName in MARKERS) {
 	let marker = MARKERS[markerName]
 	let sentence = `Merci à tou${marker}te${marker}s les contributeur${marker}trices qui sont venu${marker}e${marker}s.`
+	let aiffFile = `${markerName}.aiff`
 
-	require('child_process').exec(`say --output-file=${markerName}.aiff ${sentence}`, err => {
-		if (err)
-			console.error(err)
-		else
-			console.log(`Wrote ${markerName}.aiff`)
+	exec(`say --output-file=${aiffFile} ${sentence}`, err => {
+		if (err) return console.error(err)
+
+		console.log(`Wrote ${aiffFile}, converting to AAC`)
+
+		exec(`ffmpeg -y -i ${aiffFile} -c:a aac -b:a 64k ${markerName}.m4a`, (err, stdout, stderr) => {
+			if (err) return console.error(err)
+
+			console.log(`Converted ${aiffFile}, wrote ${markerName}.m4a`)
+			fs.unlink(aiffFile, err => {
+				if (err) return console.error(err)
+
+				console.log(`Removed ${aiffFile}`)
+			})
+		})
 	})
 }
